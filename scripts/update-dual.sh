7b273ef0d68ee2c9f694af0db54214dac3c0d6e2
Jason Chen <chezbgone@gmail.com>
2019-03-23 05:30:23 -0400

    Edit dual screen script to not mess with audio

--- scripts/update-dual.sh
+++ scripts/update-dual.sh
@@ -6,8 +6,8 @@ export XAUTHORITY=/home/jason/.Xauthority
 sleep 2
 
 xrandr | grep -q "HDMI2 connected" && xrandr --output HDMI2 --mode 1920x1080 --right-of eDP1
-xrandr | grep -q "HDMI2 connected" && pactl set-card-profile 0 output:hdmi-stereo-extra1+input:analog-stereo
+#xrandr | grep -q "HDMI2 connected" && pactl set-card-profile 0 output:hdmi-stereo-extra1+input:analog-stereo
 xrandr | grep -q "HDMI2 connected" && source ~/.fehbg
 
 xrandr | grep -q "HDMI2 disconnected" && xrandr --output HDMI2 --off
-xrandr | grep -q "HDMI2 disconnected" && pactl set-card-profile 0 output:analog-stereo+input:analog-stereo
+#xrandr | grep -q "HDMI2 disconnected" && pactl set-card-profile 0 output:analog-stereo+input:analog-stereo
